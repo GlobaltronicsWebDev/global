193b842b68bf0e7361ab3f50a9db3de5c5e2d5b1
GlobaltronicsWebDev <jimmel.bermudez@globaltronics.net>
2022-01-19 14:24:18 +0800

    edit11

--- connect.php
+++ connect.php
@@ -9,6 +9,12 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+else
+$stmt = $conn->prepare("INSERT INTO donate(FirstName,MiddleName,LastName,Email,Birthday,Address,Designation,Company,PhoneNumber)values(?,?,?,?,?,?,?,?,?,?)");
+$stmt->bind_param("ssssssssi",$FirstName,$MiddleName,$LastName,$Email,$Birthday,$Address,$Designation,$Company,$PhoneNumber);
+$stmt->execute();
 echo "Connected successfully";
+$stmt->close();
+$conn->close();
 mysqli_close($conn);
 ?>
