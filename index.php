9d62e2ca8e16233ff70befffc62b66789d2d5821
GlobaltronicsWebDev <jimmel.bermudez@globaltronics.net>
2022-01-19 13:56:02 +0800

    edit7

--- index.php
+++ index.php
@@ -15,23 +15,23 @@
     <form action="connect.php" method="POST">
         <div class="form-group">
             <label "FName">FIRSTNAME:</label>
-            <input type="FName" class="form-control" id="FirstName" placeholder="ENTER YOUR FIRSTNAME" name="FirstName">
+            <input type="FirstName" class="form-control" id="FirstName" placeholder="ENTER YOUR FIRSTNAME" name="FirstName">
             <label "FName">MIDDLENAME:</label>
-            <input type="MName" class="form-control" id="MiddleName" placeholder="ENTER YOUR MIDDLENAME" name="MiddleName">
+            <input type="MiddleName" class="form-control" id="MiddleName" placeholder="ENTER YOUR MIDDLENAME" name="MiddleName">
             <label "LName">LASTNAME:</label>
-            <input type="LName" class="form-control" id="LastName" placeholder="ENTER YOUR LASTNAME" name="LastName">
+            <input type="LastName" class="form-control" id="LastName" placeholder="ENTER YOUR LASTNAME" name="LastName">
             <label "Email">YOUR EMAIL:</label>
             <input type="Email" class="form-control" id="Email" placeholder="ENTER YOUR EMAIL ADDRESS" name="Email">
             <label "Bday">BIRTHDAY:</label>
             <input type="Birthday" class="form-control" id="Birthday" placeholder="MM/DD/YYYY" name="Birthday">
             <label "address">ADDRESS:</label>
-            <input type="address" class="form-control" id="address" placeholder="ENTER YOUR ADDRESS" name="address">
+            <input type="Address" class="form-control" id="Address" placeholder="ENTER YOUR ADDRESS" name="Address">
             <label "designation">DESIGNATION:</label>
-            <input type="designation" class="form-control" id="Email" placeholder="DESIGNATION" name="designation">
+            <input type="Designation" class="form-control" id="Designation" placeholder="DESIGNATION" name="Designation">
             <label "company">COMPANY:</label>
-            <input type="company" class="form-control" id="company" placeholder="COMPANY" name="company">
+            <input type="Company" class="form-control" id="Company" placeholder="COMPANY" name="Company">
             <label "phonenumber">PHONE NUMBER:</label>
-            <input type="phone" class="form-control" id="phone" placeholder= "PHONE NUMBER" name="PhoneNumber">
+            <input type="PhoneNumber" class="form-control" id="PhoneNumber" placeholder= "PHONE NUMBER" name="PhoneNumber">
             <button type="submit" class="btn btn-default">DONATE</button>
         </div>
     </form>
