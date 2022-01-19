25378c99a2c1a3995283cc61c99b33a9f5a97861
GlobaltronicsWebDev <jimmel.bermudez@globaltronics.net>
2022-01-19 14:27:59 +0800

    edit12

--- connect.php
+++ connect.php
@@ -3,6 +3,16 @@ $servername = "localhost";
 $database = "u553953718_testingglobal";
 $username = "u553953718_globaltronics";
 $password = "Scinort.2021";
+
+$FirstName = $_POST['FirstName'];
+$MiddleName = $_POST['MiddleName'];
+$LastName = $_POST['LastName'];
+$Email = $_POST['Email'];
+$Birthday = $_POST['Birthday'];
+$Address = $_POST['Address'];
+$Designation = $_POST['Designation'];
+$Company = $_POST['Company'];
+$PhoneNumber = $_POST['PhoneNumber'];
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
